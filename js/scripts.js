b8a306741329418b883d5aa7120ed783bbdabbd6
dunyung1 <dshege4@gmail.com>
2018-11-27 15:59:57 +0300

    change output name

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,5 @@ return  (num1 + num2);
 var num1=parseInt(prompt("Enter first: "));
 var num2=parseInt(prompt("Enter second: "));
 var answer= add(num1,num2);
-var output="Added: "+ answer;
-alert(answer);
-al
+var output="Added value: "+ answer;
+alert(output);
